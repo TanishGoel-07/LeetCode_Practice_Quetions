dd247beb55602afb476c12c453c3bc18b3af9e0c
TANISH GOEL <goeltanish2403@gmail.com>
2026-08-16 18:05:32 +0530

    Time: 2 ms (47.81%) | Memory: 11.6 MB (72.96%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>stk;
+        int maxi=0;
+        stk.push(-1);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') stk.push(i);
+            else {
+                stk.pop();
+                if(stk.empty()) stk.push(i);
+                else maxi=max(maxi,i-stk.top());
+            }
+        }
+        return maxi;
+    }
+};
