5462f967c65ff14a6a8a84baead5ea6417e8f171
TANISH GOEL <goeltanish2403@gmail.com>
2026-08-19 22:39:06 +0530

    Time: 3 ms (75.54%) | Memory: 20.2 MB (24.60%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& h) {
+    vector<int>res;
+	queue<int>q;
+    int N=numCourses;
+	vector<int>indegree(N,0);
+    vector<vector<int>>adj(numCourses);
+    for(auto it : h){
+        int u=it[0];
+        int v=it[1];
+        adj[u].push_back(v);
+    }
+		for(int i=0;i<N;i++){
+			for(auto it : adj[i]){
+				indegree[it]++;
+			}
+		}
+		for(int i=0;i<N;i++){
+			if(indegree[i]==0){
+				q.push(i);
+			}
+		}
+		while(!q.empty()){
+			int node=q.front();
+			q.pop();
+			res.push_back(node);
+			for(auto it : adj[node]){
+					indegree[it]--;
+				if(indegree[it]==0){
+					q.push(it);
+				}
+			}
+		}
+		return (res.size()==adj.size());
+    }
+};
