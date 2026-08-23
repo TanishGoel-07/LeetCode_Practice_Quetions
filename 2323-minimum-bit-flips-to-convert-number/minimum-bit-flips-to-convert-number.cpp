1d7be4bb29bb6c27568a53d49d6ef4ee6ee0e83a
TANISH GOEL <goeltanish2403@gmail.com>
2026-08-23 18:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (93.58%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int minBitFlips(int a, int b) {
-        int count = __builtin_popcount(a ^ b);
-        return count;
+        int x = a ^ b;
+    int cnt = 0;
+
+    while (x) {
+        cnt += x & 1;   // check last bit
+        x >>= 1;
+    }
+    return cnt;
     }
 };
